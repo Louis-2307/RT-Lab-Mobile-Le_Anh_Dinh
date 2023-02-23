e781813230c6f7a2eca17b47e9379d1b8538b09d
Louis-2307 <Louis.le.2307@gmail.com>
2023-02-23 10:00:49 -0500

    Initial Commit

---
+++ RT-Lab-Mobile-Le_Anh_Dinh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RT-Lab-Mobile-Le_Anh_Dinh
+//
+//  Created by Anh Dinh Le on 2023-02-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
